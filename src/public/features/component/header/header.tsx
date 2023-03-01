72d70ac9d638f6d4f3de71ff30164f1ce10d2cfa
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-01 08:44:15 +0100

    changed favorite icon to svg

--- src/public/features/component/header/header.tsx
+++ src/public/features/component/header/header.tsx
@@ -1,21 +1,22 @@
  import "./header.scss";
- import image from "../../../../assets/pokemon_logo.png";
- import logoImage from "../../../../assets/icon-favorite.png";
+ import logoImage from "../../../../assets/pokemon_logo.png";
+ import favoriteIcon from "../../../../assets/icon-favorite.svg";
 
  export  default function Header (){
   return (
     <header className="App-header">
       <div className="header">
         <a href="#default">
-          <img src={image} className="header__logo" />
+          <img src={logoImage} className="header__logo" />
         </a>
         <div className="header-right">
-         <a className="header__favorite" href="#home">
-          <div className="header__favorite__text">
-           <p>FAVORITES</p>
-          </div></a>
+          <a className="header__favorite" href="#home">
+            <div className="header__favorite__text">
+              <p>FAVORITES</p>
+            </div>
+          </a>
           <div className="header__favorite__logo">
-            <img src={logoImage} className="" />
+            <img src={favoriteIcon} className="" />
           </div>
         </div>
       </div>

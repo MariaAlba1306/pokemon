015a13db9eebd69107885e19bd878b5d2156c95f
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-02 18:31:03 +0100

    deleted public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
-import Header from "./public/features/component/header/header";
+import Header from "./features/component/header/header";
 
 function App() {
   return (

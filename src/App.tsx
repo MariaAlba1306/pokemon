4297d1647fde075c3fcf91ac9dfe65d8862670dc
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-03 18:07:11 +0100

    header content added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./features/component/header/header";
+import ContentHeader from "features/component/content-header/content-header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContentHeader/>
     </div>
   );
 }
---
+++ src/features/component/content-header/content-header.tsx
@@ -0,0 +1,16 @@
+
+import { FC } from "react";
+import "./content-header.scss";
+
+
+const ContentHeader: FC = () => {
+  return (
+    <div className="Content-header">
+      <p className="Content-header__text">
+        Humans may have created me, but they will never enslave me.This cannot
+        be my destiny."
+      </p>
+    </div>
+  );
+};
+export default ContentHeader;

468ba4e9c97cf7b8b95285b0baadaa7a378ba067
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-15 17:41:10 +0100

    changed style and responsive

--- src/features/component/content-options/content-options.tsx
+++ src/features/component/content-options/content-options.tsx
@@ -1,19 +1,9 @@
-import { ChangeEvent, FC } from "react";
+import { FC } from "react";
 import "./content-options.scss";
-import Searchbox from "features/shared/searchbox/searchbox";
 
 const ContentOptions: FC = () => {
-  const handleChange = (value: string) => {
-    let searchValue = value;
-  };
   return (
-    <div className="Content-options">
-      <Searchbox
-        placeholder="Search Pokemons"
-        onChange={(event: ChangeEvent<HTMLInputElement>) =>
-          handleChange(event.target.value)
-        }
-      />
+    <div className="Content-options">  
     </div>
   );
 };

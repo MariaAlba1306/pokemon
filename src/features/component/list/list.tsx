124a010d9a440292abc098a4beda911d9c3ee93e
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-17 19:51:15 +0100

    created component list

---
+++ src/features/component/list/list.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import "./list.scss";
+
+const Header: FC = () => {
+  return (
+    <div className="List">
+      <p>hi</p>
+    </div>
+  );
+};
+export default Header;

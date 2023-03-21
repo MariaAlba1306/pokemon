a0b0044ac2b715ea7191de537aa426707745d97c
MariaAlba1306 <maria.alba@wallapop.com>
2023-03-21 08:55:54 +0100

    added id and space for favorites

--- src/features/shared/card/card.info.ts
+++ src/features/shared/card/card.info.ts
@@ -4,17 +4,21 @@ export const cardInfo = [
     type: "Fire",
     id: "3434",
     location: "south ",
+    image:
+      "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/14.svg",
   },
   {
     name: "Pikachu",
-    type: "Fire",
+    type: "",
     id: "34546",
     location: "Spain ",
+    image:
+      "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/23.svg",
   },
   {
     name: "Bulbasour",
     type: "Fire",
     id: "34546",
-    location: "Spain ",
+    location: "",
   },
 ];
--- src/features/shared/card/card.tsx
+++ src/features/shared/card/card.tsx
@@ -1,35 +1,50 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./card.scss";
-import locationIcon from "assets/icons/location.svg";
 import { cardInfo } from "./card.info";
+import locationIcon from "assets/icons/location.svg";
+import iconFavorite from "assets/icons/icon-favorite.svg";
 
 const Card: FC = () => {
+
   return (
     <div className="Card-container">
       {cardInfo.map((info) => {
         return (
-          <div className="card">
+          <div id={info.id} className="card">
             <div className="card__image">
+              {info.image && (
+                <img
+                  src={info.image}
+                  alt="pokemon"
+                  className="card__image__space"
+                />
+              )}
               <div className="card__favorite"></div>
             </div>
             <div className="card__info">
-              <div className="card__type">
-                <p className="card__type__text">{info.type}</p>
-              </div>
-              <div className="card__id">
-                <p className="card__id__text">{info.id}</p>
-              </div>
+              {info.type && (
+                <div className="card__type">
+                  <p className="card__type__text">{info.type}</p>
+                </div>
+              )}
+              {info.id && (
+                <div className="card__id">
+                  <p className="card__id__text">{info.id}</p>
+                </div>
+              )}
             </div>
             <div className="card__name">
               <p className="card__name__text">{info.name}</p>
-              <div className="card__name__location">
-                <img
-                  src={locationIcon}
-                  className="card__name__location__icon"
-                  alt="pokemon image"
-                />
-                <p className="card__name__location__info">{info.location}</p>
-              </div>
+              {info.location && (
+                <div className="card__name__location">
+                  <img
+                    src={locationIcon}
+                    className="card__name__location__icon"
+                    alt="pokemon image"
+                  />
+                  <p className="card__name__location__info">{info.location}</p>
+                </div>
+              )}
             </div>
           </div>
         );
